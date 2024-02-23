80d36c1aad6bc2b1eceba9534213614257b09e0d
Xavier0x35e81 <xavier.0x35e81@gmail.com>
2024-02-23 11:04:33 +0800

    feat: block search function and result logging

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ docs = loader.load_and_split(text_splitter=text_splitter)
 # $$$ calculating embeddings upon function call!
 db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
 
-results = db.similarity_search_with_score(
-    "What is an interesting fact about the English language?"
-)
+# results = db.similarity_search_with_score(
+#     "What is an interesting fact about the English language?"
+# )
 
-for result in results:
-    print("==============================")
-    print(result[1])
-    print(result[0].page_content)
+# for result in results:
+#     print("==============================")
+#     print(result[1])
+#     print(result[0].page_content)
