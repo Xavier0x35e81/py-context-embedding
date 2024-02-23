59b1b8783abc0bd31cedf4f385f5b30981ffb18e
Xavier0x35e81 <xavier.0x35e81@gmail.com>
2024-02-23 10:44:55 +0800

    feat: store embeddings to ChromaDB and perform similarity search

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
 
 embeddings = OpenAIEmbeddings()
-emb = embeddings.embed_query("hey there")
-print(emb)
 
-# text_splitter = CharacterTextSplitter(
-#     separator="\n",
-#     # 200 char at most
-#     chunk_size=200,
-#     chunk_overlap=0,
-# )
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    # 200 char at most
+    chunk_size=200,
+    chunk_overlap=0,
+)
 
-# loader = TextLoader("facts.txt")
-# docs = loader.load_and_split(text_splitter=text_splitter)
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(text_splitter=text_splitter)
 
-# for doc in docs:
-#     print(doc.page_content)
-#     print("===============================")
+# $$$ calculating embeddings upon function call!
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+results = db.similarity_search_with_score(
+    "What is an interesting fact about the English language?"
+)
+
+for result in results:
+    print("==============================")
+    print(result[1])
+    print(result[0].page_content)
