71109e8a8e6b63417edb5d4d634527c122be5417
Xavier0x35e81 <xavier.0x35e81@gmail.com>
2024-03-12 14:08:24 +0800

    chore: update imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
